b2320ac2d4204478e93a79b661d796a89282399c
Pendar Ghalamchi <pendar747@gmail.com>
2020-12-07 00:49:19 +0000

    add compnoent filtering

---
+++ src/components/AntdComponents/Button.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Button from 'antd/lib/button';
+
+export default () => {
+  return (
+    <Button>Button</Button>
+  )
+}
--- src/components/ComponentLibrary/ComponentLibrary.tsx
+++ src/components/ComponentLibrary/ComponentLibrary.tsx
@@ -1,32 +1,40 @@
-import { Button } from 'antd';
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import ComponentPreview from '../ComponentPreview/ComponentPreview';
 import styles from './ComponentLibrary.less';
 import * as ElixComponents from '../ElixComponents';
-import * as Antd from 'antd';
+import * as AntdComponents from '../AntdComponents';
+import Input from 'antd/lib/input';
 
 import 'elix/define/BorderButton';
 import 'elix/define/DropdownList';
 
+const entriesToObject = (library: string) => ([name, Comp]: [string, React.FC]) => {
+  return {
+    library,
+    name,
+    Comp 
+  }
+}
+
+const allComponents = [
+  ...Object.entries(ElixComponents).map(entriesToObject('elix')), 
+  ...Object.entries(AntdComponents).map(entriesToObject('antd'))
+];
+
 const ComponentLibrary = () => {
   const [filter, setFilter] = useState<string>('');
 
-  const components = Object
-    .entries(ElixComponents)
-    .filter(([key, value]) => key.toLowerCase().indexOf(filter) == 0 && typeof value === 'function') 
-    .map(([key, value]) => ({
-      name: key,
-      Comp: value as unknown as React.FC
-    }));
+  const components = allComponents
+    .filter((component) => component.name.toLowerCase().indexOf(filter) == 0) 
 
   console.log(components);
 
   return <div>
-    <div>Search: <Antd.Input value={filter} onChange={(event) => setFilter(event.target.value)} /></div> 
+    <div><Input placeholder="filter" size="small" value={filter} onChange={(event) => setFilter(event.target.value)} /></div> 
     <div className={styles.container}>
       {
-        components.map(({ name, Comp }) => (
-          <ComponentPreview key={name} title={name}>
+        components.map(({ name, Comp, library }) => (
+          <ComponentPreview key={`${name}-${library}`} title={name}>
             <Comp>Button</Comp>
           </ComponentPreview>
         ))
