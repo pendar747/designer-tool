a548533f1339d0ea1da7e39516c3ff52ccc3e2dc
Pendar Ghalamchi <pendar747@gmail.com>
2020-12-25 22:10:04 +0000

    style library page

--- src/Pages/LibraryPage/LibraryPage.tsx
+++ src/Pages/LibraryPage/LibraryPage.tsx
@@ -1,12 +1,12 @@
-import { Button } from 'antd';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useHistory, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { fetchComponentsAction, fetchUserLibrariesAction, selectLibraryAction } from '../../state/library/actions';
 import { selectSelectedLibrary } from '../../state/library/selectors';
 import { fetchCurrentUserAction } from '../../state/user/actions';
 import venderComponents from '../../components/venderComponents';
 import Library from '../../components/Library/Library';
+import styles from './LibraryPage.less';
 
 interface LibraryPageProps {}
 
@@ -17,7 +17,6 @@ const LibraryPage: React.FC<LibraryPageProps> = () => {
   const components = venderComponents.filter(component => library?.componentIds?.includes(component.info.id));
 
   const { libraryId } = useParams<{ libraryId: string }>();
-  const history = useHistory();
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -27,8 +26,10 @@ const LibraryPage: React.FC<LibraryPageProps> = () => {
     dispatch(fetchComponentsAction.request({ libraryId }));
   }, [libraryId]);
 
-  return <div>
-    <Button onClick={() => history.push(`/library/${libraryId}/add-components`)} type="primary">Add components</Button>
+  return <div className={styles.container}>
+    <div>
+      <h1>{library?.name}</h1>
+    </div>
     <Library library={library} components={components} />
   </div>;
 }
--- src/components/Library/Library.tsx
+++ src/components/Library/Library.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import { Button, Empty, Input } from 'antd';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { removeComponentAction } from '../../state/library/actions';
 import { ComponentDefinition } from '../../types/components';
 import { Library } from '../../types/library';
 import ComponentPreview from '../ComponentPreview/ComponentPreview';
 import styles from './Library.less';
+import { SearchOutlined, PlusOutlined } from '@ant-design/icons';
 
 interface LibraryProps {
   components: ComponentDefinition[],
@@ -14,22 +17,45 @@ interface LibraryProps {
 const Library: React.FC<LibraryProps> = ({ components, library }) => {
 
   const dispatch = useDispatch();
+  const history = useHistory();
+  const [filter, setFilter] = useState<string>('');
 
   const onRemove = (componentId: string) => {
     dispatch(removeComponentAction.request({ componentId, libraryId: library.id }))
   }
 
-  return <div className={styles.container}>
-    {components.map(component => (
-      <ComponentPreview 
-        onRemove={() => onRemove(component.info.id)} 
-        key={component.info.id} 
-        title={component.info.name} 
-        isAdded={true} 
-        onAdd={() => {}}>
-        <component.Demo />
-      </ComponentPreview>
-    ))}
+  const onAddComponents = () => history.push(`/library/${library.id}/add-components`);
+
+  return <div>
+    <div className={styles.toolbar}>
+      <div>
+        <Input 
+          suffix={<SearchOutlined />}
+          onChange={event => setFilter(event.target.value)} 
+          className={styles.filterInput} placeholder="filter..." />
+      </div>
+      <Button icon={<PlusOutlined />} onClick={onAddComponents} type="primary">Add components</Button>
+    </div>
+    {
+      components.length == 0
+        ? <div className={styles.empty}>
+          <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="There are no components in this library." />
+        </div>
+        : <div className={styles.components}>
+          {components
+            .filter(({ info }) => filter.length > 0 ? info.name.indexOf(filter) === 0 : true)
+            .map(component => (
+            <ComponentPreview 
+              onRemove={() => onRemove(component.info.id)} 
+              key={component.info.id} 
+              title={component.info.name} 
+              isAdded={true} 
+              onAdd={() => {}}>
+              <component.Demo />
+            </ComponentPreview>
+          ))}
+        </div>
+    }
   </div>;
 }
 
